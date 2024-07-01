901b885c9a91098bebfc3c0cd391ebcd4c2e9eb5
KM4ACK <jason.oleham@gmail.com>
2024-07-01 07:13:31 -0500

    add wwl check

--- install.sh
+++ install.sh
@@ -3,15 +3,26 @@
 #28JUNE2024 KM4ACK
 APP_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+clear;echo;echo
+echo "installing posit-generator"
+echo "################################"
+echo "# enter sudo password if asked #"
+echo "################################"
 mkdir -p $HOME/bin/posit-generator
 chmod +x ${APP_DIR}/2M-pos ${APP_DIR}/hf-pos ${APP_DIR}/pos-generator ${APP_DIR}/pota-pos ${APP_DIR}/repeater-pos
 cp -r ${APP_DIR}/* $HOME/bin/posit-generator/
 rm $HOME/bin/posit-generator/install.sh
 
+ck=$(which wwl)
+if [ -z ${ck} ]; then
+    sudo apt update
+    sudo apt install wwl
+fi
+
 cat <<EOF >/run/user/$UID/pos-generator.desktop
 [Desktop Entry]
-Name=POS Generator
-GenericName=POS Generator
+Name=POSIT Generator
+GenericName=POSIT Generator
 Comment=POSIT Generator for YAAC
 Exec=$HOME/bin/posit-generator/pos-generator
 Icon=$HOME/YAAC/images/doityourself.png
@@ -23,6 +34,7 @@ sudo mv /run/user/$UID/pos-generator.desktop /usr/local/share/applications/
 
 rm -rf ${APP_DIR}
 cd
+echo;echo "installation done. use menu to start application POSIT-Generator"
 
 
 
