aaf0fe30ede183887052a80cf65a5625c2c79c99
KM4ACK <jason.oleham@gmail.com>
2024-07-01 07:00:04 -0500

    update

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ APP_DIR="$(cd "$(dirname "$0")" && pwd)"
 mkdir -p $HOME/bin/posit-generator
 chmod +x ${APP_DIR}/2M-pos ${APP_DIR}/hf-pos ${APP_DIR}/pos-generator ${APP_DIR}/pota-pos ${APP_DIR}/repeater-pos
 cp -r ${APP_DIR}/* $HOME/bin/posit-generator/
+rm $HOME/bin/posit-generator/install.sh
 
 cat <<EOF >/run/user/$UID/pos-generator.desktop
 [Desktop Entry]
@@ -21,5 +22,7 @@ EOF
 sudo mv /run/user/$UID/pos-generator.desktop /usr/local/share/applications/
 
 rm -rf ${APP_DIR}
+cd
+
 
 
